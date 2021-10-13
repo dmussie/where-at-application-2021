21f412d4dda4fb0fb884607405195aa2ce5a6330
Danny Mussie <dannymussie7@gmail.com>
2021-10-13 16:22:06 -0500

    modified concerts.router.js to contain an axios.get request for songkick api data and added that router to server.js

--- server/routes/concerts.router.js
+++ server/routes/concerts.router.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+
+router.get('/:search', (req, res) => {
+    console.log('req.params are:', req.params.search);
+    axios.get(`https://api.songkick.com/api/3.0/search/artists.json?apikey=${process.env.SONGKICK_API_KEY}&query=${req.params.search}`)
+    .then(response => {
+        res.send(response.data)
+    })
+    .catch(error => {
+        console.log(error);
+    }); 
+});
+
+module.exports = router;
