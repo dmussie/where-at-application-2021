3ccb2731c2b778cf0a832c44590f57cc8cd5277c
Danny Mussie <dannymussie7@gmail.com>
2021-10-13 13:09:26 -0500

    updated RegisterForm.jsx to include additional registration input fields

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,6 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zip, setZip] = useState('');
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -52,6 +59,78 @@ function RegisterForm() {
         </label>
       </div>
       <div>
+        <label htmlFor="first_name">
+          First Name:
+          <input
+            type="first_name"
+            name="first_name"
+            value={first_name}
+            required
+            onChange={(event) => setState(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="last_name">
+          Last Name:
+          <input
+            type="last_name"
+            name="last_name"
+            value={last_name}
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="city">
+          City:
+          <input
+            type="city"
+            name="city"
+            value={city}
+            required
+            onChange={(event) => setCity(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="state">
+          State:
+          <input
+            type="state"
+            name="state"
+            value={state}
+            required
+            onChange={(event) => setState(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="zip">
+          Zip:
+          <input
+            type="zip"
+            name="zip"
+            value={zip}
+            required
+            onChange={(event) => setZip(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>  
         <input className="btn" type="submit" name="submit" value="Register" />
       </div>
     </form>
