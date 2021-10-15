20f7b4b4544ca9052b59f2bb16bbbc25ca7c9b52
Danny Mussie <dannymussie7@gmail.com>
2021-10-15 14:05:38 -0500

    created and populated concerts.saga.js file, FETCH_CONCERTS & SET_CONCERTS actions established in saga

---
+++ src/redux/sagas/concerts.saga.js
@@ -0,0 +1,24 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+// concerts saga: will be fired on "FETCH_CONCERTS" actions
+function* fetchConcerts() {
+    try{
+        const config = {
+            headers: {'Content-Type': 'application/json'},
+            withCredentials: true,
+        };
+
+        const response = yield axios.get('/api/concerts', config);
+
+        yield put({ type: 'SET_CONCERTS', payload: response.data });
+    } catch (error) {
+        console.log('Concerts get request failed', error);
+    }
+}
+
+function* concertsSaga() {
+    yield takeLatest('FETCH_CONCERTS', fetchConcerts);
+}
+
+export default concertsSaga;
