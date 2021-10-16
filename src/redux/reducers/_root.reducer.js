b1d333ac8e4b8dee7414cf8731b97d34a935f61f
Danny Mussie <dannymussie7@gmail.com>
2021-10-16 15:56:38 -0500

    added venuesReducer to rootReducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import concertsReducer from './concerts.reducer';
+import venuesReducer from './venues.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,6 +13,7 @@ import concertsReducer from './concerts.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
+  venuesReducer, // contains venue table information from database to aid in event searches
   concertsReducer // contains concert event information that a user wants to save
 });
 
