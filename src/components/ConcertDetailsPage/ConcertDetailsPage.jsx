0223aa6838dd759ab032d6da9997b324d1cc88d9
Danny Mussie <dannymussie7@gmail.com>
2021-10-18 15:01:13 -0500

    created axios post request to post relevant concert info to database

--- src/components/ConcertDetailsPage/ConcertDetailsPage.jsx
+++ src/components/ConcertDetailsPage/ConcertDetailsPage.jsx
@@ -10,15 +10,34 @@ function ConcertDetailsPage() {
     const concerts = useSelector(store => store.concertDetailsReducer);
     console.log(concerts);
 
+    const postConcert = () => {
+        axios({
+            method: 'POST',
+            url: '/api/concerts',
+            data: {
+                displayName: concerts.displayName,
+                city: concerts.location.city,
+                time: concerts.start.time,
+                uri: concerts.uri
+            }
+        }).then((response) => {
+            console.log('Event post successful');
+            alert('Concert Saved!')
+        }).catch((error) => {
+            alert('Oh No! Could Not Save Event.');
+        })
+        history.push('/userconcerts');
+    }
+
     return(
         <>
             <h3>Here's Some More Info On The Show!</h3>
-            <p>{concerts.displayName}</p>
-            <p>{concerts.location.city}</p>
-            <p>{concerts.start.date}</p>
-            <p>{concerts.start.time}</p>
-            <p>{concerts.uri}</p>
-            <button>Save Event!</button>
+            Event: <p>{concerts.displayName}</p>
+            City: <p>{concerts.location.city}</p>
+            Date: <p>{concerts.start.date}</p>
+            Time: <p>{concerts.start.time}</p>
+            Event Page: <p>{concerts.uri}</p>
+            <button onClick={postConcert}>Save Event!</button>
         </>
     )
 }
