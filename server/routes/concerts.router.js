e61d2bb557a27a75f544698a9fed39a178b6e067
Danny Mussie <dannymussie7@gmail.com>
2021-10-20 21:13:45 -0500

    created a router.delete route in concerts.router.js

--- server/routes/concerts.router.js
+++ server/routes/concerts.router.js
@@ -95,4 +95,14 @@ router.get('/', (req, res) => {
       })
   });
 
+  router.delete('/:id', (req, res) => {
+    const queryText = 'DELETE FROM "events" WHERE id=$1';
+    pool.query(queryText, [req.params.id])
+      .then(() => { res.sendStatus(200); })
+      .catch((err) => {
+        console.log('Error completing SELECT plant query', err);
+        res.sendStatus(500);
+      });
+  });
+
 module.exports = router;
