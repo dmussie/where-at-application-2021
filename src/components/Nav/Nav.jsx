0c97bff5220fbb467698a9962f1f177bc635b4f3
Danny Mussie <dannymussie7@gmail.com>
2021-10-24 11:57:52 -0500

    changed font family for the application

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Where At?</h2>
+        <h3 className="nav-title">Where At?</h3>
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
