d848bccc6f1896d194d2eac5b0261b9380528196
Danny Mussie <dannymussie7@gmail.com>
2021-10-30 11:43:13 -0500

    changed message on about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,12 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p> Where At? at it's core is a one-stop-shop in the process of tracking concerts. It's the premier "search, 
+          survey, and save" concert finder to suit your needs! Find your next concert now!
+        </p>
+        <p> Technologies employed: 
+          Javascript, React.js, Redux-Sagas.js, Node.js, Express, postgreSQL, Material UI, and the Songkick API
+        </p>
       </div>
     </div>
   );
